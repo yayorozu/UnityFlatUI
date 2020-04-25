fc01889d514b660ac0fdd17dbfd38af226baca8f
hatch <hatch.tech.eng@gmail.com>
2020-04-26 02:46:41 +0900

    ボタンとして使えるサンプル

---
+++ Sample/FlatUISample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FlatUISample : MonoBehaviour
+{
+	[SerializeField]
+	private Button _button;
+
+	private void Awake()
+	{
+		_button.onClick.AddListener(Click);
+	}
+
+	private void Click()
+	{
+		Debug.Log("Click");
+	}
+}
